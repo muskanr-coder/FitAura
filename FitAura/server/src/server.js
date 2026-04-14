cbcd2dc4a8416c29840bcef7013280c7f49b0997
muskanr-coder <muskanr2107@gmail.com>
2026-04-14 11:47:56 +0530

    Update server.js

--- FitAura/server/src/server.js
+++ FitAura/server/src/server.js
@@ -1,14 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
+// Load env variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
 app.use(cors({
-  origin: "https://fit-aura-8spm.vercel.app",  // <-- yahan apna Vercel domain
+  origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
 app.use(express.json());
 
-import userRoutes from "./routes/userRoutes.js";
+// Routes
 app.use("/api/users", userRoutes);
-
-import productRoutes from "./routes/productRoutes.js";
 app.use("/api/products", productRoutes);
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => {
+    console.log("DB Error:", err);
+    process.exit(1);
+  });
+
+// Server start
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
